465eec523e7d9a6353dd3c8583dce0a6e78d5a7f
AlexadreRochaQ <alexandrerochagyn@gmail.com>
2025-12-18 18:58:40 +0100

    Feat: Bouton ajouter/supprimer

--- client/src/components/pokemon-card.tsx
+++ client/src/components/pokemon-card.tsx
@@ -9,6 +9,8 @@ import {
   DialogTitle,
   DialogTrigger,
 } from './ui/dialog'
+import { Button } from './ui/button'
+import { apiClient } from '@/services/api-client'
 
 interface IPokemonCardProps {
   pokemon: IPokemon
@@ -17,12 +19,17 @@ interface IPokemonCardProps {
 export const PokemonCard: FC<IPokemonCardProps> = ({ pokemon }): JSX.Element => {
   // push to /pokemon/:name au click
 
+const ajouter = async () => {
+  const response = await apiClient.post('/api/dresseur/69440d02b8b5e71344daaa3e/pokedex/add', pokemon)
+  console.log(response)
+  console.log(pokemon)
+}
   return (
     <Dialog>
       <DialogTrigger asChild>
         <Card className='cursor-pointer hover:bg-accent/50 transition-colors duration-100'>
           <CardHeader>
-            <CardTitle>{pokemon.name}</CardTitle>
+            <CardTitle>{pokemon.name}</CardTitle><p>#{pokemon.id}</p>
             <CardDescription>
               <p>{pokemon.types.join(', ')}</p>
             </CardDescription>
@@ -30,9 +37,16 @@ export const PokemonCard: FC<IPokemonCardProps> = ({ pokemon }): JSX.Element => 
           <CardContent>
             <img src={pokemon.image} alt={pokemon.name} width={96} height={96} />
           </CardContent>
-          <CardFooter>
-            <p>#{pokemon.id}</p>
-          </CardFooter>
+          <CardFooter className='flex justify-between items-center'>
+          <Button
+            onClick={(e) => {
+              e.stopPropagation()
+              ajouter()
+            }}
+          >
+            ajouter
+          </Button>
+        </CardFooter>
         </Card>
       </DialogTrigger>
       <DialogContent>
